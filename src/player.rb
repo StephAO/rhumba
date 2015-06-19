1b61ddfae5e72cd65d469d055c56d4fbbdc532c6
miguelarocao <mig_ao@live.com>
2015-06-18 20:21:37 -0700

    boards: fixed and tested start position generation player: added snake generation classes snake: edited initialization

--- src/player.rb
+++ src/player.rb
@@ -1,6 +1,11 @@
+require_relative 'Snake'
+
 class Player
 
-  attr_reader :name
+  attr_reader :name,:my_snake
+
+  #Class variables
+  @@num_players=0
 
   Data = Struct.new(:wins, :losses, :deaths, :avg_points) do
     def print
@@ -8,10 +13,15 @@ class Player
     end
   end
 
-  def initialize(_name, _id)
+  def initialize(_name)
     @name = _name #players name (string)
-    @id = _id     #players id (int)
+    @id = @@num_players     #players id (int)
     @data = Data.new(0,0,0,0)
+    @@num_players+=1
+  end
+
+  def gen_snake(init_loc)
+    @my_snake=Snake.new(init_loc)
   end
 
   def next_move()
