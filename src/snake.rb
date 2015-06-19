c7e08bd6fbb1bef90fccdfcfe3ddc3299ac68ef5
miguelarocao <mig_ao@live.com>
2015-06-18 22:16:41 -0700

    snake: removed alive/death rules, added "in_snake" board: finished gen_start function

--- src/snake.rb
+++ src/snake.rb
@@ -5,24 +5,26 @@ class Snake
 
   attr_reader :score, :position
 
+  #Class constant
+  ALLOWED_MOVES||=['up','down','left','right']
+
   def initialize(init_loc)
     @score = 0
-    @position = FIFO.new([init_loc])
+    @position = FIFO.new(init_loc)
     @alive = true
   end
 
   def move(new_pos, food = false, occupied = false)
-    if not occupied and @alive
+    if @alive
       @position.insert(new_pos, food)
       @score += @position.size
-    else
-      @alive = false
     end
   end
 
   def next_coordinate(direction)
     direction.downcase!
-    x, y = @position.first
+    x = @position.first.x
+    y = @position.first.y
     case direction
       when 'left'
         x -= 1
@@ -34,12 +36,19 @@ class Snake
         y -= 1
     end
     new_coord = Coordinates.new(x,y)
-    if @position.include(new_coord)
-      puts 'dead'
-      @alive = false
-    else
-      new_coord
-    end
+  end
+
+  def in_snake(coord)
+    #checks if input coordinate is in snake
+    @position.include(coord)
+  end
+
+  def get_head
+    @position.first
+  end
+
+  def get_tail
+    @position.last
   end
 
   def find_next_move(board)
